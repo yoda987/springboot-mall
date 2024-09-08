b094e659f55a8ddd76287a96e143f68c98760d78
LAPTOP-5H341LVT\User <yoda.251604@gmail.com>
2024-09-08 17:59:32 +0800

    Update to jpa

--- src/main/java/com/yoda/springbootmall/controller/ProductController.java
+++ src/main/java/com/yoda/springbootmall/controller/ProductController.java
@@ -1,97 +1,82 @@
 package com.yoda.springbootmall.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.yoda.springbootmall.dto.ProductRequest;
 import com.yoda.springbootmall.model.Product;
-import com.yoda.springbootmall.service.ProductService;
-
-import jakarta.validation.Valid;
+import com.yoda.springbootmall.repository.ProductRepository;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 
 	@Autowired
-	private ProductService productService;
-	
-	@GetMapping("/products/{productId}")
-	public ResponseEntity<Product> getProduct(@PathVariable Integer productId){
-		Product product = productService.getProductById(productId);
-		
-		if(product != null) {
-			return ResponseEntity.status(HttpStatus.OK).body(product);
-		}else {
-			
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	private ProductRepository productRepository;
+
+	@GetMapping("/{productId}")
+	public ResponseEntity<Product> getProduct(@PathVariable Integer productId) {
+
+		Product product = productRepository.findById(productId).orElse(null);
+		if (product != null) {
+			return ResponseEntity.ok(product);
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 		}
 	}
-	
-	@PostMapping("/products")
-	public ResponseEntity<Product> createProduct(@RequestBody @Valid ProductRequest productRequest){
-		Integer productId = productService.createProduct(productRequest);
-		
-		Product product = productService.getProductById(productId);
-		
-		return ResponseEntity.status(HttpStatus.CREATED).body(product);
-	}
-	
-	@PutMapping("/products/{productId}")
-	public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
-												 @RequestBody @Valid ProductRequest productRequest){
-				
-		//檢查product是否存在
-		Product product = productService.getProductById(productId);
-		
-		if(product == null) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+	@GetMapping
+	public ResponseEntity<List<Product>> getAllProducts() {
+		List<Product> products = (List<Product>) productRepository.findAll();
+		if (products != null) {
+			return ResponseEntity.ok(products);
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 		}
-				
-		//修改商品資訊
-		productService.updateProduct(productId, productRequest);
-		
-		Product updateProduct = productService.getProductById(productId);
-		
-		return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
-		
 	}
-	
-	
-	@DeleteMapping("/products/delete/{productId}")
-	public String deleteProduct(@PathVariable Integer productId){
-				
-			
-		if(productService.deleteProduct(productId) == productId) {
-			return "刪除成功";
-		}else {		
-			return "商品Id錯誤";
-		}	
-			
-			
-			
-//		productService.deleteProduct(productId);
-			
-//		if(productId == productService.deleteProduct(productId)) {
-//			return ResponseEntity.status(HttpStatus.OK).body(productId);
-//		}else {
-//			
-//			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-//		}
-		
+
+	@PostMapping
+	public ResponseEntity<String> createProduct(@RequestBody Product product) {
+
+		productRepository.save(product);
+		System.out.println("新增成功");
+		return ResponseEntity.status(HttpStatus.CREATED).body("新增成功");
 	}
-		
-	
-	
-	
+
+	@PutMapping("/{productId}")
+	public ResponseEntity<String> updateProduct(@PathVariable Integer productId, @RequestBody Product product) {
+
+		if (productRepository.existsById(productId)) {
+			product.setProductId(productId);
+			productRepository.save(product);
+			System.out.println("Update成功");
+			return ResponseEntity.ok("Update成功");
+		} else {
+			System.out.println("Update失敗");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("商品 Id 錯誤");
+		}
+	}
+
+	@DeleteMapping("/{productId}")
+	public ResponseEntity<String> deleteProduct(@PathVariable Integer productId) {
+
+		if (productRepository.existsById(productId)) {
+			productRepository.deleteById(productId);
+			System.out.println("刪除成功");
+			return ResponseEntity.ok("刪除成功");
+		} else {
+			System.out.println("刪除失敗");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("商品 Id 錯誤");
+		}
+	}
 }
--- src/main/java/com/yoda/springbootmall/model/Product.java
+++ src/main/java/com/yoda/springbootmall/model/Product.java
@@ -1,75 +1,127 @@
 package com.yoda.springbootmall.model;
 
-import java.sql.Date;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
 public class Product {
 
-	private Integer productId;
-	private String productName;
-	private String category;
-	private String imageUrl;
-	private Integer price;
-	private Integer stock;
-	private String description;
-	private Date createdDate;
-	private Date lastModifiedDate;
-	
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer productId;
+
+    @Column(name="productName")
+    private String productName;
+
+    @Column(name="category")
+    private String category;
+
+    @Column(name="imageUrl")
+    private String imageUrl;
+
+    @Column(name="price")
+    private Integer price;
+
+    @Column(name="stock")
+    private Integer stock;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="createdDate")
+    private LocalDateTime createdDate;
+
+    @Column(name="lastModifiedDate")
+    private LocalDateTime lastModifiedDate;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdDate = now;
+        this.lastModifiedDate = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.lastModifiedDate = LocalDateTime.now();
+    }
+
+    
+    
 	public Integer getProductId() {
 		return productId;
 	}
+
 	public void setProductId(Integer productId) {
 		this.productId = productId;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+
 	public String getCategory() {
 		return category;
 	}
+
 	public void setCategory(String category) {
 		this.category = category;
 	}
+
 	public String getImageUrl() {
 		return imageUrl;
 	}
+
 	public void setImageUrl(String imageUrl) {
 		this.imageUrl = imageUrl;
 	}
+
 	public Integer getPrice() {
 		return price;
 	}
+
 	public void setPrice(Integer price) {
 		this.price = price;
 	}
+
 	public Integer getStock() {
 		return stock;
 	}
+
 	public void setStock(Integer stock) {
 		this.stock = stock;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public Date getCreatedDate() {
+
+	public LocalDateTime getCreatedDate() {
 		return createdDate;
 	}
-	public void setCreatedDate(Date createdDate) {
+
+	public void setCreatedDate(LocalDateTime createdDate) {
 		this.createdDate = createdDate;
 	}
-	public Date getLastModifiedDate() {
+
+	public LocalDateTime getLastModifiedDate() {
 		return lastModifiedDate;
 	}
-	public void setLastModifiedDate(Date lastModifiedDate) {
+
+	public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
 		this.lastModifiedDate = lastModifiedDate;
 	}
-	
 	
 	
 	
